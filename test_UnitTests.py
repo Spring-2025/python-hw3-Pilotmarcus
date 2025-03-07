71d9eb48d6d7104f1d883b2bc95f56b816e90629
Pilotmarcus <150866809+Pilotmarcus@users.noreply.github.com>
2025-03-06 19:23:53 -0500

    Update test_UnitTests.py

--- test_UnitTests.py
+++ test_UnitTests.py
@@ -24,21 +24,4 @@ def test_YahooData2returns():
     assert np.isclose(returns[1], -0.00970874, atol=0.01)
     assert np.isclose(returns[2],  0.01960784, atol=0.01)
     
-import VaR_File
-def test_VaR():
-    r = np.random.normal(0.05, 0.03, 1000000)
-    probability2SD = 0.977249868
-    percent_var = VaR_File.VaR(r, probability2SD)
-    assert np.round(percent_var, 2) == 0.01
 
-import ES_File
-def test_ES():
-    u = np.random.uniform(0, 100, 100000)
-
-    # Test the ES function with an alpha of 0.8
-    es_alpha = ES_File.ES(losses=u, alpha=0.8)
-    assert np.round(es_alpha, 0) == 90
-    
-    # Test the ES function with a VaR of 80
-    es_var = ES_File.ES(losses=u, VaR=80)
-    assert np.round(es_var, 0) == 90
