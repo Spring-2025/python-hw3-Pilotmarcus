e082cf10fa7f2bde3e20d75845cf1e6a9e0170f7
Pilotmarcus <150866809+Pilotmarcus@users.noreply.github.com>
2025-03-06 19:33:27 -0500

    Update VaR_File.py

--- VaR_File.py
+++ VaR_File.py
@@ -2,7 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
 
-def VaR(r, confidence, principal=1):
-    alpha = 1 - confidence
-    var_value = np.percentile(r, alpha * 100)
-    return abs(var_value) * principal 
+def VaR(r,confidence,principal=1):
+  
+  var_percentile=np.percentile(r,(1-confidence)*100)
+  VaR_value=abs(var_percentile)*principal
+  return VaR_value
+
+def percent_var(r,confidence):
+  
+  plt.hist(r,bins=50,alpha=0.75)
+  plt.show()
+
+  out=np.percentile(r,(1-confidence)*100)
+  return abs(out)
